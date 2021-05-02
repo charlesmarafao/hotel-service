d840a67c905ae5820027f553912b10c6c8d1d2bc
charlesmarafao <charlesmarafao@gmail.com>
2021-05-02 04:39:22 -0300

    chore: use env port

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,6 +1,6 @@
 /* eslint-disable no-console */
 import { app } from './app';
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log('Server Running on port 3000');
 });
