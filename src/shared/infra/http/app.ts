9aa7300d5a244c98eac92199111140eee308dc45
charlesmarafao <charlesmarafao@gmail.com>
2021-05-03 14:47:22 -0300

    chore: add open api doc

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -1,15 +1,18 @@
 /* eslint-disable import/prefer-default-export */
 import 'reflect-metadata';
 import 'express-async-errors';
+import swaggerUi from 'swagger-ui-express';
 
 import express from 'express';
 import cors from 'cors';
 import routes from './routes';
+import swaggerFile from '../../../docs/swagger.json';
 
 import '@shared/infra/typeorm';
 import '@shared/container';
 
 const app = express();
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.use(cors());
 app.use(express.json());
